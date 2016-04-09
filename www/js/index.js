83c325741db1a5ea38e94ccea4c1576021fe7bd9
x2rcaddie <cowboypga@aol.com>
2016-04-08 20:12:38 -0700

    Geo Location plugin
    Geo Location plugin

--- www/js/index.js
+++ www/js/index.js
@@ -53,4 +53,29 @@ var ref = window.open('http://www.shawacademy.com', '_blank', 'hidden=yes');
 // some time later...
 ref.show();
 }
-    
+    function geo(){
+
+        // onSuccess Callback
+        // This method accepts a Position object, which contains the
+        // current GPS coordinates
+        //
+        var onSuccess = function(position) {
+            alert('Latitude: '          + position.coords.latitude          + '\n' +
+                  'Longitude: '         + position.coords.longitude         + '\n' +
+                  'Altitude: '          + position.coords.altitude          + '\n' +
+                  'Accuracy: '          + position.coords.accuracy          + '\n' +
+                  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                  'Heading: '           + position.coords.heading           + '\n' +
+                  'Speed: '             + position.coords.speed             + '\n' +
+                  'Timestamp: '         + position.timestamp                + '\n');
+        };
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                  'message: ' + error.message + '\n');
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);    	    	   	
+    }
